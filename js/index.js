e7dce3a88b6232edfecd37ce4129379dcd0022ca
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-11 14:47:03 -0600

    Completed assignment

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
 function greetProgrammer() {
   console.log("Hello, programmer!");
 }
-
+greetProgrammer()
 /*
   You should be able to call this function with one argument and see its output in the terminal:
   greet("Naureen");
@@ -15,7 +15,7 @@ function greetProgrammer() {
 function greet(name) {
   console.log(`Hello, ${name}!`);
 }
-
+greet('Sergio')
 /*
   You should be able to call this function with no arguments and see its output in the terminal:
   greetWithDefault();
@@ -28,7 +28,7 @@ function greet(name) {
 function greetWithDefault(name = "programmer") {
   console.log(`Hello, ${name}!`);
 }
-
+greetWithDefault('Link')
 /*
   You should be able to call this function with two arguments and get back its return value:
   const sum = add(1, 2);
@@ -38,7 +38,7 @@ function greetWithDefault(name = "programmer") {
 function add(num1, num2) {
   return num1 + num2;
 }
-
+console.log(add(3,8))
 /*
   You should be able to call this function with two arguments and get back its return value:
   const result = halve(4);
@@ -54,3 +54,5 @@ function halve(number) {
 
   return number / 2;
 }
+console.log(halve('Sergio'))
+console.log(halve(67))
